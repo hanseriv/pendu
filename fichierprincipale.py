9b7fa9f3038ccebba40c2cc52890ec7a45f9b35e
jean TROUSSIER <jean.troussier@cpe.fr>
2024-10-03 10:04:35 +0200

    improving commit

--- fichierprincipale.py
+++ fichierprincipale.py
@@ -6,5 +6,29 @@ to do :
 """
 import mainfunction as mf
 
+unsigned_int_live = 0
+list_char_lettre_du_mot = []
+list_char_lettre_joueur = []
+list_char_lettre_joueur_fausse = []
+unsigned_int_indicator_victory = 0
+as_it_began = False
+
 while(1):
-    
+    if as_it_began == False:
+        unsigned_int_indicator_victory,unsigned_int_live,list_char_lettre_du_mot,list_char_lettre_joueur,list_char_lettre_joueur_fausse = mf.initialiser(unsigned_int_indicator_victory,unsigned_int_live,list_char_lettre_du_mot,list_char_lettre_joueur,list_char_lettre_joueur_fausse)
+        as_it_began = True
+        list_char_lettre_du_mot = mf.list_char_word_seperator(mf.string_word_chooser(mf.list_string_mot("dictionnaire/dico.txt")),list_char_lettre_du_mot)[:]
+        mf.list_char_letter_starter(list_char_lettre_du_mot,list_char_lettre_joueur)
+    elif mf.bool_death_tester(unsigned_int_live) == True:
+        print("vous avez perdu...")
+        break
+    elif mf.bool_word_found(unsigned_int_indicator_victory,list_char_lettre_du_mot) == True:
+        print("vous avez gagné")
+        break
+    else :
+        mf.void_afficher_mot(unsigned_int_indicator_victory, list_char_lettre_joueur, list_char_lettre_joueur_fausse,list_char_lettre_du_mot)
+        char_letter_chosen = input("veuillez entrer une lettre :")
+        while mf.bool_is_it_a_char(char_letter_chosen) != True:
+            print("la lettre entrée n'est pas valide...")
+            char_letter_chosen = input("veuillez entrer une lettre :")
+        unsigned_int_live,list_char_lettre_joueur = mf.void_letter_checker_function(char_letter_chosen,unsigned_int_live,list_char_lettre_joueur,list_char_lettre_du_mot,list_char_lettre_joueur_fausse)
--- mainfunction.py
+++ mainfunction.py
@@ -4,19 +4,38 @@ date : jeudi 3 octobre
 obj :  fichier fonction du pendu textuelle
 to do :
 """
-
 import random as rd
-unsigned_int_live = 0
-list_char_lettre_du_mot = []
-list_char_lettre_joueur = []
-list_char_lettre_joueur_fausse = []
-unsigned_int_indicator_victory = 0
 
-def void_afficher_mot():
+
+def bool_is_it_a_char(string):
+    if len(string) == 1:
+        return True
+    else:
+        return False
+
+def void_letter_checker_function(char_letter_to_be_tested,unsigned_int_live,list_char_lettre_joueur,list_char_lettre_du_mot,list_char_lettre_joueur_fausse):
+    if char_letter_to_be_tested in list_char_lettre_du_mot and char_letter_to_be_tested not in list_char_lettre_joueur:
+        print("vous avez trouvez une nouvelle lettre")
+        list_char_lettre_joueur.append(list_char_lettre_joueur)
+    elif (char_letter_to_be_tested not in list_char_lettre_du_mot and char_letter_to_be_tested in list_char_lettre_joueur_fausse) or char_letter_to_be_tested in list_char_lettre_joueur:
+        print("la lettre que vous avez rentré a deja été utilisé")
+    else:
+        unsigned_int_live += 1
+    return unsigned_int_live, list_char_lettre_joueur
+
+def initialiser(unsigned_int_indicator_victory,unsigned_int_live,list_char_lettre_du_mot,list_char_lettre_joueur,list_char_lettre_joueur_fausse):
+    unsigned_int_live = 0
+    list_char_lettre_du_mot = []
+    list_char_lettre_joueur = []
+    list_char_lettre_joueur_fausse = []
+    unsigned_int_indicator_victory = 0
+    return unsigned_int_indicator_victory,unsigned_int_live,list_char_lettre_du_mot,list_char_lettre_joueur,list_char_lettre_joueur_fausse
+
+
+def void_afficher_mot(unsigned_int_indicator_victory, list_char_lettre_joueur, list_char_lettre_joueur_fausse,list_char_lettre_du_mot):
     """
     fonction qui n'a aucune entré et qui affiche les lettres trouvés et cache celle qui ne l'ont pas été encore
     """
-    global unsigned_int_indicator_victory 
     unsigned_int_indicator_victory = 0
     for char_lettre in list_char_lettre_du_mot:
         if char_lettre in list_char_lettre_joueur:
@@ -24,7 +43,10 @@ def void_afficher_mot():
             unsigned_int_indicator_victory += 1
         else:
             print("_", end='')
+    print("lettre fausse: ",list_char_lettre_joueur_fausse)
     print(" ")
+
+
 
 def list_string_mot(stream_file):
     """
@@ -46,25 +68,26 @@ def string_word_chooser(list_string_file):
     """
     unsigned_int_index = rd.randint(1,len(list_string_file)) - 1
     string_word_to_guess = list_string_file[unsigned_int_index]
+    print(string_word_to_guess)
     return string_word_to_guess
 
-def list_char_word_seperator(string_word):
+def list_char_word_seperator(string_word,list_char_lettre_du_mot):
     """
     input : string_word
     fonction qui transforme une string en liste de char
     """
     for char_buffer in string_word:
         list_char_lettre_du_mot.append(char_buffer)
-    
-def list_char_letter_starter(string_word_to_guess):
+    return list_char_lettre_du_mot
+
+def list_char_letter_starter(list_char_lettre_du_mot,list_char_lettre_joueur):
     """
-    input : string_word_to_guess
     fonction qui met choisie "aléatoirement" une lettre et l'ajoute dans la liste du joueur
     """
-    unsigned_int_index = rd.randint(1,len(string_word_to_guess)) - 1
-    list_char_lettre_joueur.append(string_word_to_guess[unsigned_int_index])
+    unsigned_int_index = rd.randint(1,len(list_char_lettre_du_mot)) - 1
+    list_char_lettre_joueur.append(list_char_lettre_du_mot[unsigned_int_index])
 
-def bool_death_tester():
+def bool_death_tester(unsigned_int_live):
     """
     fonction qui vérifie que le joueur n'a pas perdu
     """
@@ -73,7 +96,7 @@ def bool_death_tester():
     else :
         return False
     
-def bool_word_found():
+def bool_word_found(unsigned_int_indicator_victory,list_char_lettre_du_mot):
     """
     fonction qui vérifie si le joueur n'a pas gagner
     """
